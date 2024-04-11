baea3b65420a226651cbb889ad5aba2482b90a1f
netherquark <netherquark@protonmail.com>
2024-04-11 15:59:25 +0530

    solve R q1_a: f,g,j

--- R/q1_a_ts_r_func.R
+++ R/q1_a_ts_r_func.R
@@ -2,24 +2,18 @@ library(dplyr)
 library(moments)
 library(lubridate)
 
-# Function to analyze time-series dataset
 analyze_time_series <- function(dataset) {
   dataset$Date <- as.Date(dataset$Date, format = "%d.%m.%Y")
   
-  # Calculate mean, median, standard deviation, variance for 'Open'
   mean_value <- mean(dataset$Open)
   median_value <- median(dataset$Open)
   std_dev <- sd(dataset$Open)
   variance <- var(dataset$Open)
-  
-  # Calculate skewness and kurtosis for 'Open'
+
   skewness_kurtosis <- c(skewness(dataset$Open), kurtosis(dataset$Open))
-  
-  # Calculate quartiles for 'Open'
+
   quartiles <- quantile(dataset$Open, c(0, 0.25, 0.5, 0.75, 1))
-  
-  # Calculate Pearson's correlation coefficient between 'Date' and 'Open'
-  # correlation_coefficient <- cor(as.numeric(dataset$Date), dataset$Open, method = "pearson")
+
   correlation_coefficient <- cor(dataset$Open[-1], dataset$Open[-nrow(dataset)], method = "pearson")
 
   # Return results
@@ -33,7 +27,20 @@ analyze_time_series <- function(dataset) {
   return(analysis_results)
 }
 
-# Example usage of functions
 time_series_data <- read.csv("/home/netherquark/Soham/Soham/Studies/Bachelors/SIT B.Tech (AI-ML)/first_year/Sem-II/STAIML/STAIML-CA/R/beijing_multi_site_air_quality_data.csv", header = TRUE, stringsAsFactors = FALSE)
 analysis_results <- analyze_time_series(time_series_data)
 print(analysis_results)
+
+# Function to visualize data
+visualize_data <- function(dataset) {  
+  # Create scatter plot
+  plot(dataset$Open, dataset$Volume, main = "Scatter Plot of Volume over Time", xlab = "Date", ylab = "Volume", col = "blue")
+  
+  # Create box plot
+  boxplot(dataset$Open, main = "Box Plot of Volume", ylab = "Volume", col = "lightgreen")
+  
+  # Create histogram
+  hist(dataset$Open, breaks = 20, main = "Histogram of Volume", xlab = "Volume", col = "skyblue", border = "black")
+}
+
+visualization_results <- visualize_data(time_series_data)
